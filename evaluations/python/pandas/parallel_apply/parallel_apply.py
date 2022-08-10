8432c4c9b7320b19fcde8b58956f98080ed7d0b4
Nikhil Gupta <33585645+ngupta23@users.noreply.github.com>
2022-08-10 05:18:06 -0500

    cleanup with black formatting

--- evaluations/python/pandas/parallel_apply/parallel_apply.py
+++ evaluations/python/pandas/parallel_apply/parallel_apply.py
@@ -8,17 +8,17 @@ from multiprocessing import Pool, cpu_count
 import pandas as pd
 from pandas.core.groupby.generic import DataFrameGroupBy
 
+
 def apply_grouped_parallel_with_args(
     grouped_data: DataFrameGroupBy,
     func: Callable,
-    unpacked:bool=False,
-    reset_inner_index:bool=False,
+    unpacked: bool = False,
+    reset_inner_index: bool = False,
     n_jobs: Optional[int] = None,
     **kwargs
-    ) -> Union[Tuple, pd.Series, pd.DataFrame]:       
-    
-    """Function to parallelize the Pandas `apply` method on a `groupby` object 
+) -> Union[Tuple, pd.Series, pd.DataFrame]:
 
+    """Function to parallelize the Pandas `apply` method on a `groupby` object
     Parameters
     ----------
     grouped_data : DataFrameGroupBy
@@ -36,60 +36,64 @@ def apply_grouped_parallel_with_args(
         Number of cores to use. If None, uses 90% of cores., by default None
     **kwargs: optional
         Additional arguments to be passed to `func`
-
     Returns
     -------
     Union[Tuple, pd.Series, pd.DataFrame]
         Depends on the output of `func` and the arguments selected
-    """  
-      
+    """
+
     # https://stackoverflow.com/questions/18204782/runtimeerror-on-windows-trying-python-multiprocessing
     # https://stackoverflow.com/questions/39686957/parallelize-groupby-with-multiple-arguments
-    
+
     if n_jobs:
-        p = Pool(n_jobs)    
+        p = Pool(n_jobs)
     else:
         p = Pool(round(cpu_count() * 0.9))
     ret_list = p.map(partial(func, **kwargs), [group for _, group in grouped_data])
     p.close()
-    
+
     # ret_list will have one object per group
     # If the function is returning a single value, then the object will be a DataFrame per group
-    # If the function is returning multiple Dataframes, then the object will be a Tuple of DataFrames
+    # If the function is returning multiple Data frames, then the object will be a Tuple of DataFrames
     # We need to handle these situations appropriately
     indices = list(grouped_data.groups.keys())
-    
+
     # Function 'func' is returning 1 DataFrame per group
     if isinstance(ret_list[0], (pd.DataFrame, pd.Series)):
         # Returning Value: https://stackoverflow.com/questions/26187759/parallelize-apply-after-pandas-groupby
-        return_obj = pd.concat(ret_list, axis = 0)
-        return return_obj 
-      
+        return_obj = pd.concat(ret_list, axis=0)
+        return return_obj
+
     # Function 'func' is returning a tuple of values per group
     # Each value could be a DataFrame, Series or something else
     if isinstance(ret_list[0], tuple):
         return_obj = pd.Series(ret_list)
         return_obj.index = indices
-        
+
         if not unpacked:
-            return return_obj  
-        
-        
+            return return_obj
+
         def unpack(col, index):
-            # If the initial unpack returns a series of DataFrames (one dataframe for each group)
-            # then we need to further unpack the dataframes and concatenate them together
+            # If the initial unpack returns a series of DataFrames (one data frame for each group)
+            # then we need to further unpack the data frames and concatenate them together
             if isinstance(col, pd.Series) and isinstance(col.iloc[0], pd.DataFrame):
                 temp = [col.iloc[i] for i in range(len(col))]
-                temp = pd.concat(temp, keys=index, names=['group'], axis=0)
+                temp = pd.concat(temp, keys=index, names=["group"], axis=0)
                 return temp
-            return col 
-            
+            return col
+
         def return_col(data, col_index):
             # Lambda function was giving this error: https://jira.sonarsource.com/browse/SONARPY-587
             return data[col_index]
-            
-        initial_unpack = [return_obj.apply(return_col, col_index=j) for j in range(len(return_obj[0]))]
+
+        initial_unpack = [
+            return_obj.apply(return_col, col_index=j) for j in range(len(return_obj[0]))
+        ]
         final_unpack = [unpack(item, index=indices) for item in initial_unpack]
         return final_unpack
-    
-    raise(TypeError("Currently only supports functions that return a Pandas DataFrame, Pandas Series or a Tuple of values"))  
+
+    raise (
+        TypeError(
+            "Currently only supports functions that return a Pandas DataFrame, Pandas Series or a Tuple of values"
+        )
+    )
